5d556ea7a626b7ddcf576c7425096b6d9780430b
Sonia <soniatoledo294@hotmail.com>
2018-04-24 19:13:48 -0400

    Plantilla basica con Bootstrap 4

---
+++ Listado.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<style>
+ 
+
+
+
+</style>
+</head>
+<body>
+
+<nav class="navbar navbar-light bg-light navbar-expand-md">
+<a class="navbar-brand"  href="#">My Portfolio  </a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+
+<div class="collapse navbar-collapse" id="navbarNav">
+<div class="navbar-nav">
+<div class="navbar-item">
+<a class="nav-link" href="#">HomePage</a>
+</div>
+
+<div class="navbar-item"><a class="nav-link" href="#">Blog</a>
+</div>
+
+<div class="navbar-item"><a class="nav-link" href="#">About</a>
+</div>
+
+<div class="navbar-item"><a class="nav-link" href="#">Contact us</a></div>
+
+</div>
+</div>
+
+</nav>
+
+
+
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+<h1 class="display-3">Welcome to my website</h1>
+<p class="lead">I'm a developer and designer. Check my
+portfolio below</p>
+</div>
+</div>
+
+
+
+<div class="container">
+<div class="row">
+
+<div class="col-6 col-sm-6 col-lg-3">
+<div class="card mt-3">
+<img  class="card-img-top" alt="Card header image" src="resources/Abyssinian.ico"/>
+<div class="card-body">
+<h5 class="card-title">Project 1</h5>
+<p class="card-text"> An awesome project</p>
+<a class="btn btn-info" href="#">See project</a>
+</div>
+</div>
+</div>
+
+
+
+<div class="col-6 col-sm-6 col-lg-3">
+<div class="card mt-3">
+<img  class="card-img-top" alt="Card header image" src="resources/American shorthair.ico"/>
+<div class="card-body">
+<h5 class="card-title">Project 2</h5>
+<p class="card-text"> An awesome project</p>
+<a class="btn btn-info" href="#">See project</a>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="col-6 col-sm-6 col-lg-3">
+<div class="card mt-3">
+<img  class="card-img-top" alt="Card header image" src="resources/Bombay.ico"/>
+<div class="card-body">
+<h5 class="card-title">Project 3</h5>
+<p class="card-text"> An awesome project</p>
+<a class="btn btn-info" href="#">See project</a>
+</div>
+</div>
+</div>
+
+
+<div class="col-6 col-sm-6 col-lg-3">
+<div class="card mt-3">
+<img  class="card-img-top" alt="Card header image" src="resources/White Persian.ico"/>
+<div class="card-body">
+<h5 class="card-title">Project 4</h5>
+<p class="card-text"> An awesome project</p>
+<a class="btn btn-info" href="#">See project</a>
+</div>
+</div>
+</div>
+
+</div>
+
+<div class="row">
+<div class="col-12 col-sm-12 col-md-6 offset-md-3">
+<h3>Reach out!</h3>
+<form>
+<div class="form-group"> <input type="text" class="form-control" id="email" placeholder="Your email..."/></div>
+<div class="form-group"> <textarea class="form-control" placeholder="Your message..."></textarea></div>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+</html>
